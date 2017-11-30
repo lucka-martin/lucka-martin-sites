c10360e041e7d9204b653ed4ebdb0622eaf42e7e
martin.janys <martin.janys@gmail.com>
2017-11-30 20:50:58 +0100

    home button

---
+++ src/components/HomeButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header" class="hide">
+    <div class="inner">
+      <router-link to="/" class="button hidden"></router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component
+  export default class HomeButton extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <HomeButton></HomeButton>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+  import HomeButton from '@/components/HomeButton.vue'
+
+  @Component({
+    components: {
+      HomeButton
+    }
+  })
+  export default class HomePage extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
