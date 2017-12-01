7ba69fbf4f927166d5b5a33c8fec4f95b7ad8698
martin.janys <martin.janys@gmail.com>
2017-12-01 07:03:37 +0100

    rename column to column item

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,60 +4,60 @@
     <div id="main">
       <Columns>
         <!-- Column 1 (horizontal, vertical, horizontal, vertical) -->
-        <Column>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic01.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic02.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic03.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic04.jpg" alt="" /></a>
-        </Column>
+        </ColumnItem>
 
         <!-- Column 2 (vertical, horizontal, vertical, horizontal) -->
-        <Column>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic06.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic05.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic08.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic07.jpg" alt="" /></a>
-        </Column>
+        </ColumnItem>
 
         <!-- Column 3 (horizontal, vertical, horizontal, vertical) -->
-        <Column>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic09.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic12.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic11.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic10.jpg" alt="" /></a>
-        </Column>
+        </ColumnItem>
 
         <!-- Column 4 (vertical, horizontal, vertical, horizontal) -->
-        <Column>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic13.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic14.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic15.jpg" alt="" /></a>
-        </Column>
-        <Column>
+        </ColumnItem>
+        <ColumnItem>
           <a href="detail1.html"><img src="images/pic16.jpg" alt="" /></a>
-        </Column>
+        </ColumnItem>
       </Columns>
     </div>
   </MainLayout>
@@ -69,14 +69,14 @@
   import MainLayout from '@/layouts/MainLayout.vue'
   import HomeButton from '@/components/HomeButton.vue'
   import Columns from '@/components/Columns.vue'
-  import Column from '@/components/Column.vue'
+  import ColumnItem from '@/components/ColumnItem.vue'
 
   @Component({
     components: {
       MainLayout,
       HomeButton,
       Columns,
-      Column
+      ColumnItem
     }
   })
   export default class HomePage extends Vue {
