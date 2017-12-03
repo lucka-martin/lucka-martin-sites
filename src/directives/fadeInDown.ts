a6081d60c131c98e25739093827f23b52b529d2f
martin.janys <martin.janys@gmail.com>
2017-12-03 14:14:53 +0100

    refactoring component

--- src/directives/fadeInDown.ts
+++ src/directives/fadeInDown.ts
@@ -1,7 +1,14 @@
-export default {
-  inserted: function (el: HTMLElement) {
-    el.classList.add('fadeInDown');
-    el.classList.add('animated');
-    el.style.animationDelay = (Math.random() / 2) + "s";
-  }
+import { DirectiveOptions, DirectiveFunction } from "vue";
+
+class FadeInDownDirective implements DirectiveOptions {
+
+  get inserted(): DirectiveFunction {
+    return (el) => {
+      el.classList.add('fadeInDown');
+      el.classList.add('animated');
+      el.style.animationDelay = (Math.random() / 2) + "s"
+    }
+  };
 }
+
+export default new FadeInDownDirective();
